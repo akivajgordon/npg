2482f84c640bf98d85bce1a90afd513bb4560c1c
Akiva Gordon <akivag29@gmail.com>
2024-07-17 01:47:44 -0400

    Add gitignore and npm init templates

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 
-import { existsSync, mkdirSync } from 'fs'
+import {
+  copyFileSync,
+  existsSync,
+  mkdirSync,
+  readFileSync,
+  writeFileSync,
+} from 'fs'
 import { execSync } from 'node:child_process'
-import { basename, resolve } from 'path'
+import path, { basename, resolve } from 'path'
 
 // Get the project name from command line arguments
 const projectPath = process.argv[2]
@@ -33,4 +39,27 @@ try {
   console.log('Initialized a new Git repository.')
 } catch (error) {
   console.error('Error initializing Git repository:', error)
+}
+
+// Copy the gitignore template to the new project directory
+const gitignoreTemplatePath = resolve(__dirname, '../templates/gitignore')
+const gitignorePath = resolve(projectPath, '.gitignore')
+
+try {
+  copyFileSync(gitignoreTemplatePath, gitignorePath)
+  console.log('Added .gitignore file.')
+} catch (error) {
+  console.error('Error copying .gitignore file:', error)
+}
+
+try {
+  // Initialize a new package.json file in the new project directory
+  const initFile = resolve(__dirname, '../templates/npm-init.json')
+  const npmInitContents = readFileSync(initFile, 'utf8')
+
+  writeFileSync(path.join(projectPath, 'package.json'), npmInitContents)
+  console.log('Initialized a new package.json file.')
+} catch (err) {
+  console.error('Error initializing package.json:', err)
+  process.exit(1)
 }
