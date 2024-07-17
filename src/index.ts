3aaf7e650cef72aaae2f14a10d2f2da52c3571a2
Akiva Gordon <akivag29@gmail.com>
2024-07-17 10:14:42 -0400

    Refactor to clean up main

--- src/index.ts
+++ src/index.ts
@@ -10,56 +10,79 @@ import {
 import { execSync } from 'node:child_process'
 import path, { basename, resolve } from 'path'
 
-// Get the project name from command line arguments
-const projectPath = process.argv[2]
+const main = () => {
+  const projectPath = getProjectPath()
 
-if (!projectPath) {
-  console.error('Please provide a project name.')
-  process.exit(1)
+  makeProjectDir(projectPath)
+
+  initializeGit(projectPath)
+
+  addGitIgnore(projectPath)
+
+  npmInit(projectPath)
 }
 
-const projectName = basename(projectPath)
+main()
 
-// Resolve the full path of the new project directory
-const resolvedProjectPath = resolve(process.cwd(), projectPath)
+function npmInit(projectPath: string) {
+  try {
+    const initFile = resolve(__dirname, '../templates/npm-init.json')
+    const npmInitContents = readFileSync(initFile, 'utf8')
 
-if (existsSync(resolvedProjectPath)) {
-  console.error(`Error: Directory or file "${projectPath}" already exists.`)
-  process.exit(1)
+    writeFileSync(path.join(projectPath, 'package.json'), npmInitContents)
+    console.log('Initialized a new package.json file.')
+  } catch (err) {
+    console.error('Error initializing package.json:', err)
+    process.exit(1)
+  }
 }
 
-// Create the new project directory
-mkdirSync(resolvedProjectPath)
-console.log(
-  `Project directory "${projectName}" created at ${resolvedProjectPath}.`,
-)
+function addGitIgnore(projectPath: string) {
+  // Copy the gitignore template to the new project directory
+  const gitignoreTemplatePath = resolve(__dirname, '../templates/gitignore')
+  const gitignorePath = resolve(projectPath, '.gitignore')
 
-try {
-  execSync('git init', { cwd: resolvedProjectPath })
-  console.log('Initialized a new Git repository.')
-} catch (error) {
-  console.error('Error initializing Git repository:', error)
+  try {
+    copyFileSync(gitignoreTemplatePath, gitignorePath)
+    console.log('Added .gitignore file.')
+  } catch (error) {
+    console.error('Error copying .gitignore file:', error)
+  }
 }
 
-// Copy the gitignore template to the new project directory
-const gitignoreTemplatePath = resolve(__dirname, '../templates/gitignore')
-const gitignorePath = resolve(projectPath, '.gitignore')
-
-try {
-  copyFileSync(gitignoreTemplatePath, gitignorePath)
-  console.log('Added .gitignore file.')
-} catch (error) {
-  console.error('Error copying .gitignore file:', error)
+function initializeGit(projectPath: string) {
+  try {
+    execSync('git init', { cwd: projectPath })
+    console.log('Initialized a new Git repository.')
+  } catch (error) {
+    console.error('Error initializing Git repository:', error)
+  }
 }
 
-try {
-  // Initialize a new package.json file in the new project directory
-  const initFile = resolve(__dirname, '../templates/npm-init.json')
-  const npmInitContents = readFileSync(initFile, 'utf8')
+function makeProjectDir(projectPath: string) {
+  const projectName = basename(projectPath)
 
-  writeFileSync(path.join(projectPath, 'package.json'), npmInitContents)
-  console.log('Initialized a new package.json file.')
-} catch (err) {
-  console.error('Error initializing package.json:', err)
-  process.exit(1)
+  // Create the new project directory
+  mkdirSync(projectPath)
+  console.log(`Project directory "${projectName}" created at ${projectPath}.`)
+}
+
+function getProjectPath() {
+  // Get the project name from command line arguments
+  const projectPath = process.argv[2]
+
+  if (!projectPath) {
+    console.error('Please provide a project name.')
+    process.exit(1)
+  }
+
+  // Resolve the full path of the new project directory
+  const resolvedProjectPath = resolve(process.cwd(), projectPath)
+
+  if (existsSync(resolvedProjectPath)) {
+    console.error(`Error: Directory or file "${projectPath}" already exists.`)
+    process.exit(1)
+  }
+
+  return resolvedProjectPath
 }
